766330ab6afcd8b49d3e608aa88f665d57df0e85
Administrator <fwef>
2020-04-22 10:45:40 +0800

    commit

--- vendor/topthink/think-captcha/src/config.php
+++ vendor/topthink/think-captcha/src/config.php
@@ -5,9 +5,9 @@
 
 return [
     //验证码位数
-    'length'   => 4,
+    'length'   => 5,
     // 验证码字符集合
-    'codeSet'  => '0123456789',
+    'codeSet'  => '2345678abcdefhijkmnpqrstuvwxyzABCDEFGHJKLMNPQRTUVWXY',
     // 验证码过期时间
     'expire'   => 1800,
     // 是否使用中文验证码
@@ -19,11 +19,11 @@ return [
     //验证码字符大小
     'fontSize' => 25,
     // 是否使用混淆曲线
-    'useCurve' => false,
+    'useCurve' => true,
     //是否添加杂点
-    'useNoise' => false,
+    'useNoise' => true,
     // 验证码字体 不设置则随机
-    'fontttf'  => '4.ttf',
+    'fontttf'  => '',
     //背景颜色
     'bg'       => [243, 251, 254],
     // 验证码图片高度
