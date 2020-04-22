eb50a7fd8cf4b0684a4b1ff61107130d1d6059c8
Administrator <fwef>
2020-04-22 10:47:19 +0800

    commit

--- config/captcha.php
+++ config/captcha.php
@@ -5,9 +5,9 @@
 
 return [
     //验证码位数
-    'length'   => 5,
+    'length'   => 4,
     // 验证码字符集合
-    'codeSet'  => '2345678abcdefhijkmnpqrstuvwxyzABCDEFGHJKLMNPQRTUVWXY',
+    'codeSet'  => '0123456789',
     // 验证码过期时间
     'expire'   => 1800,
     // 是否使用中文验证码
@@ -19,11 +19,11 @@ return [
     //验证码字符大小
     'fontSize' => 25,
     // 是否使用混淆曲线
-    'useCurve' => true,
+    'useCurve' => false,
     //是否添加杂点
-    'useNoise' => true,
+    'useNoise' => false,
     // 验证码字体 不设置则随机
-    'fontttf'  => '',
+    'fontttf'  => '2.ttf',
     //背景颜色
     'bg'       => [243, 251, 254],
     // 验证码图片高度
